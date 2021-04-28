512024b9a1c508b38c040137aef5cf5e3b3ee9d1
Thewakers2012 <1518832673@qq.com>
2021-04-28 15:15:18 +0800

    新建 Article 模型

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,8 +1,20 @@
 package types
 
-import "strconv"
+import (
+	"goblog/pkg/logger"
+	"strconv"
+)
 
 // Int64ToString 将 int64 型转换为 string 类型
 func Int64ToString(num int64) string {
 	return strconv.FormatInt(num, 10)
+}
+
+// StringToInt 将字符串转化为 int
+func StringToInt(str string) int {
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		logger.LogError(err)
+	}
+	return i
 }

adaec6f3e0674a16dd30aa97f4ee992f6ce01697
Thewakers2012 <1518832673@qq.com>
2021-05-03 22:08:31 +0800

    文章作者

--- app/models/article/curd.go
+++ app/models/article/curd.go
@@ -10,16 +10,17 @@ import (
 func Get(idstr string) (Article, error) {
 	var article Article
 	id := types.StringToInt(idstr)
-	if err := model.DB.First(&article, id).Error; err != nil {
+	if err := model.DB.Preload("User").First(&article, id).Error; err != nil {
 		return article, err
 	}
+
 	return article, nil
 }
 
-// GetAll 获取全部的文章
+// GetAll 获取全部文章
 func GetAll() ([]Article, error) {
 	var articles []Article
-	if err := model.DB.Find(&articles).Error; err != nil {
+	if err := model.DB.Debug().Preload("User").Find(&articles).Error; err != nil {
 		return articles, err
 	}
 	return articles, nil

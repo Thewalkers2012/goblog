228ffecb18e751ee6bc38c7091b0f0cff207a8db
Thewakers2012 <1518832673@qq.com>
2021-05-03 21:29:44 +0800

    配置和环境变量

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
+	"goblog/config"
+	c "goblog/pkg/config"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,10 +12,15 @@ import (
 
 var router *mux.Router
 
+func init() {
+	// 初始化配置信息
+	config.Initialize()
+}
+
 func main() {
 	// 路由的初始化
 	bootstrap.SetupDB()
 	router = bootstrap.SetupRoute()
 	// 通过命名路由获取 URL 实例
-	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+	http.ListenAndServe(":"+c.GetString("app.port"), middlewares.RemoveTrailingSlash(router))
 }
